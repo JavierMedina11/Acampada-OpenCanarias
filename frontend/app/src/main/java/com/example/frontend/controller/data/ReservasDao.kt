d9885927fe774df18e0d9016f845562df290fdaa
JavierMedina11 <javierinformatica1311@gmail.com>
2021-04-16 18:36:34 +0200

    ALL OK !!!

--- frontend/app/src/main/java/com/example/frontend/controller/data/ReservasDao.kt
+++ frontend/app/src/main/java/com/example/frontend/controller/data/ReservasDao.kt
@@ -17,13 +17,13 @@ interface ReservasDao {
 
     // SON ESTAS DE AQUI ABAJO
 
-    @Query("SELECT * FROM reservas  where id_zona = :zoneId  AND checkin =:check AND fecha_entrada = :fecha ")
+    @Query("SELECT * FROM reservas  where id_zona = :zoneId AND fecha_entrada = :fecha  AND checkin =:check")
     fun getByDateChecked(zoneId: Int, check: String, fecha: String): LiveData<List<Reserva>>
 
-    @Query("SELECT * FROM reservas  where id_zona = :zoneId AND checkin =:check AND fecha_entrada = :fecha OR fecha_entrada = :fecha2 OR fecha_entrada = :fecha3")
+    @Query("SELECT * FROM reservas  where id_zona = :zoneId AND checkin =:check AND (fecha_entrada = :fecha OR fecha_entrada = :fecha2 OR fecha_entrada = :fecha3)")
     fun getByDateCheckedPer3Days(zoneId: Int, check: String, fecha: String, fecha2: String, fecha3: String): LiveData<List<Reserva>>
 
-    @Query("SELECT * FROM reservas  where id_zona = :zoneId AND checkin =:check AND fecha_entrada = :fecha OR fecha_entrada = :fecha2 OR fecha_entrada = :fecha3 OR fecha_entrada = :fecha4 OR fecha_entrada = :fecha5")
+    @Query("SELECT * FROM reservas  where id_zona = :zoneId AND checkin =:check AND (fecha_entrada = :fecha OR fecha_entrada = :fecha2 OR fecha_entrada = :fecha3 OR fecha_entrada = :fecha4 OR fecha_entrada = :fecha5)")
     fun getByDateCheckedPer5Days(zoneId: Int, check: String, fecha: String, fecha2: String, fecha3: String, fecha4: String, fecha5: String): LiveData<List<Reserva>>
 
     // SON ESTAS DE AQUI ARRIBA
